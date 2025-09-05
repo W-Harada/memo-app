23473f37ed3a6b06e9f204c7d2f98d7f90bff0b4
Wako-Harada <wako.harada.0709@gmail.com>
2025-09-05 15:21:51 +0900

    add Header.vue

---
+++ laravel/resources/js/components/Header.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Image from "./svgs/DocumentSvg.vue";
+</script>
+
+<template>
+    <div class="text-center text-lg text-orange-400">
+        <Image/>
+        <h1>かんたんメモ</h1>
+    </div>
+    <div class="text-center text-sm text-gray-500">
+        <p>思いついたことをサッと記録しよう</p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
