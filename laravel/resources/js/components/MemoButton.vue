c8271a1e9eb01c033a5c305b5ed7026b7acba478
Wako-Harada <wako.harada.0709@gmail.com>
2025-09-10 21:43:32 +0900

    create

--- laravel/resources/js/components/MemoButton.vue
+++ laravel/resources/js/components/MemoButton.vue
@@ -3,7 +3,6 @@ import Image from "./svgs/PlusSvg.vue";
 const props = defineProps<{ memo:string }>()
 const emit = defineEmits(['save'])
 import { computed } from "vue"
-import { ref } from "vue"
 const isDisabled = computed(() => props.memo.trim().length === 0)
 </script>
 
@@ -11,8 +10,8 @@ const isDisabled = computed(() => props.memo.trim().length === 0)
     <button type="button"
             @click="emit('save')"
             :disabled="isDisabled"
-            :class="['flex justify-center items-center gap-2 w-11/12 h-14 border-solid rounded-md p-4 ml-6',props.memo.trim().length > 0?'bg-orange-500':'bg-orange-300']">
-        <Image class="text-white"/>
+            :class="['text-white flex justify-center items-center gap-2 w-11/12 h-14 border-solid rounded-md p-4 ml-6',props.memo.trim().length > 0?'bg-orange-500':'bg-orange-300']">
+        <Image/>
         メモを保存
     </button>
 </template>
